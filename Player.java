475cf403b3decfe55b2822417f663b30e2e8c97e
Dexter Gramfors <dexter.gramfors@gmail.com>
2014-09-03 16:53:39 +0200

    Started implementing minimax.

--- Player.java
+++ Player.java
@@ -24,7 +24,52 @@ public class Player {
          * Here you should write your algorithms to get the best next move, i.e.
          * the best next state. This skeleton returns a random move instead.
          */
-        Random random = new Random();
-        return lNextStates.get(random.nextInt(lNextStates.size()));
+        int bestValue = Integer.MIN_VALUE;
+        GameState bestState = lNextStates.get(0);
+        for (GameState state : lNextStates) {
+            int value = miniMax(state, 30, true);
+            if (value > bestValue) {
+                bestValue = value;
+                bestState = state;
+            }
+        }
+        return bestState;
+    }
+
+    private int heuristicValue(GameState whiteState, boolean player1) {
+        GameState state = null;
+        if (player1)
+            state = whiteState;
+        else
+            state = state.reversed();
+        if (state.isWhiteWin())
+            return Integer.MAX_VALUE;
+        if (state.isRedWin())
+            return Integer.MIN_VALUE;
+
+        return 0;
+    }
+
+    private int miniMax (GameState node, int depth, boolean maximizing) {
+        if (depth == 0 || node.isEOG())
+            return heuristicValue(node, maximizing); // Heuristic value of node (state)
+        int bestValue;
+        if (maximizing) {
+            bestValue = Integer.MIN_VALUE;
+            Vector<GameState> children = new Vector<GameState>();
+            for (GameState child : children) {
+                int val = miniMax(child, depth - 1, false);
+                bestValue = Math.max(bestValue, val);
+            }
+            return bestValue;
+        } else {
+            bestValue = Integer.MAX_VALUE;
+            Vector<GameState> children = new Vector<GameState>();
+            for (GameState child : children) {
+                int val = miniMax(child, depth - 1, true);
+                bestValue = Math.min(bestValue, val);
+            }
+            return bestValue;
+        }
     }
 }
